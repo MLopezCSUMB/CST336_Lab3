f3867fa34c04fef70ee164401d1f9b07cd32c492
Miguel Lopez <mnl30@aol.com>
2016-09-22 05:47:43 +0000

    added images for players and players.php which has players info

---
+++ players.php
@@ -0,0 +1,24 @@
+<?php
+
+$players = array(
+        array(
+            'name' => 'Kip', 
+            'image' => '<img src="imgs/Kip.jpg">',
+            ),
+        array(
+            'name' => 'Bob Ross',
+            'image' => '<img src="imgs/bob_ross.jpg">',
+            ),
+        array(
+            'name' => 'Eleven',
+            'image' => '<img src="imgs/eleven.jpg">',
+            ),
+        array(
+            'name' => 'Kenny Powers',
+            'image' => '<img src="imgs/kenny.jpg">',
+            )
+        );
+        
+        var_dump($players);
+
+?>
