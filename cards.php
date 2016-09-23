58391278566b5da20dbe0e79b67bd5b51c819198
Miguel Lopez <mnl30@aol.com>
2016-09-23 04:23:01 +0000

    Added the array of cards with values and image link

---
+++ cards.php
@@ -0,0 +1,216 @@
+<?php
+
+$cards = array(
+    array(
+        'value' => 1,
+        'image' => '<img src="cards/clubs/1.png">',
+        ),
+    array(
+        'value' => 2, 
+        'image' => '<img src="cards/clubs/2.png">',
+        ),
+    array(
+        'value' => 3, 
+        'image' => '<img src="cards/clubs/3.png">',
+        ),
+    array(
+        'value' => 4, 
+        'image' => '<img src="cards/clubs/4.png">',
+        ),
+    array(
+        'value' => 5, 
+        'image' => '<img src="cards/clubs/5.png">',
+        ),
+    array(
+        'value' => 6, 
+        'image' => '<img src="cards/clubs/6.png">',
+        ),
+    array(
+        'value' => 7, 
+        'image' => '<img src="cards/clubs/7.png">',
+        ),
+    array(
+        'value' => 8, 
+        'image' => '<img src="cards/clubs/8.png">',
+        ),
+    array(
+        'value' => 9, 
+        'image' => '<img src="cards/clubs/9.png">',
+        ),
+    array(
+        'value' => 10, 
+        'image' => '<img src="cards/clubs/10.png">',
+        ),
+    array(
+        'value' => 11, 
+        'image' => '<img src="cards/clubs/11.png">',
+        ),
+    array(
+        'value' => 12, 
+        'image' => '<img src="cards/clubs/12.png">',
+        ),
+    array(
+        'value' => 13, 
+        'image' => '<img src="cards/clubs/13.png">',
+        ),
+    array(
+        'value' => 1, 
+        'image' => '<img src="cards/diamonds/1.png">',
+        ),
+    array(
+        'value' => 2, 
+        'image' => '<img src="cards/diamonds/2.png">',
+        ),
+    array(
+        'value' => 3, 
+        'image' => '<img src="cards/diamonds/3.png">',
+        ),
+    array(
+        'value' => 4, 
+        'image' => '<img src="cards/diamonds/4.png">',
+        ),
+    array(
+        'value' => 5, 
+        'image' => '<img src="cards/diamonds/5.png">',
+        ),
+    array(
+        'value' => 6, 
+        'image' => '<img src="cards/diamonds/6.png">',
+        ),
+    array(
+        'value' => 7, 
+        'image' => '<img src="cards/diamonds/7.png">',
+        ),
+    array(
+        'value' => 8, 
+        'image' => '<img src="cards/diamonds/8.png">',
+        ),
+    array(
+        'value' => 9, 
+        'image' => '<img src="cards/diamonds/9.png">',
+        ),
+    array(
+        'value' => 10, 
+        'image' => '<img src="cards/diamonds/10.png">',
+        ),
+    array(
+        'value' => 11, 
+        'image' => '<img src="cards/diamonds/11.png">',
+        ),
+    array(
+        'value' => 12, 
+        'image' => '<img src="cards/diamonds/12.png">',
+        ),
+    array(
+        'value' => 13, 
+        'image' => '<img src="cards/diamonds/13.png">',
+        ),
+    array(
+        'value' => 1, 
+        'image' => '<img src="cards/hearts/1.png">',
+        ),
+    array(
+        'value' => 2, 
+        'image' => '<img src="cards/hearts/2.png">',
+        ),
+    array(
+        'value' => 3, 
+        'image' => '<img src="cards/hearts/3.png">',
+        ),
+    array(
+        'value' => 4, 
+        'image' => '<img src="cards/hearts/4.png">',
+        ),
+    array(
+        'value' => 5, 
+        'image' => '<img src="cards/hearts/5.png">',
+        ),
+    array(
+        'value' => 6, 
+        'image' => '<img src="cards/hearts/6.png">',
+        ),
+    array(
+        'value' => 7, 
+        'image' => '<img src="cards/hearts/7.png">',
+        ),
+    array(
+        'value' => 8, 
+        'image' => '<img src="cards/hearts/8.png">',
+        ),
+    array(
+        'value' => 9, 
+        'image' => '<img src="cards/hearts/9.png">',
+        ),
+    array(
+        'value' => 10, 
+        'image' => '<img src="cards/hearts/10.png">',
+        ),
+    array(
+        'value' => 11, 
+        'image' => '<img src="cards/hearts/11.png">',
+        ),
+    array(
+        'value' => 12, 
+        'image' => '<img src="cards/hearts/12.png">',
+        ),
+    array(
+        'value' => 13, 
+        'image' => '<img src="cards/hearts/13.png">',
+        ),
+    array(
+        'value' => 1, 
+        'image' => '<img src="cards/spades/1.png">',
+        ),
+    array(
+        'value' => 2, 
+        'image' => '<img src="cards/spades/2.png">',
+        ),
+    array(
+        'value' => 3, 
+        'image' => '<img src="cards/spades/3.png">',
+        ),
+    array(
+        'value' => 4, 
+        'image' => '<img src="cards/spades/4.png">',
+        ),
+    array(
+        'value' => 5, 
+        'image' => '<img src="cards/spades/5.png">',
+        ),
+    array(
+        'value' => 6, 
+        'image' => '<img src="cards/spades/6.png">',
+        ),
+    array(
+        'value' => 7, 
+        'image' => '<img src="cards/spades/7.png">',
+        ),
+    array(
+        'value' => 8, 
+        'image' => '<img src="cards/spades/8.png">',
+        ),
+    array(
+        'value' => 9, 
+        'image' => '<img src="cards/spades/9.png">',
+        ),
+    array(
+        'value' => 10, 
+        'image' => '<img src="cards/spades/10.png">',
+        ),
+    array(
+        'value' => 11, 
+        'image' => '<img src="cards/spades/11.png">',
+        ),
+    array(
+        'value' => 12, 
+        'image' => '<img src="cards/spades/12.png">',
+        ),
+    array(
+        'value' => 13, 
+        'image' => '<img src="cards/spades/13.png">',
+        ),    
+    );
+
+var_dump($cards);
+
+?>
